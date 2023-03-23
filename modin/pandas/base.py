5390610627ac196d638ec8c3e8f844d335f5989e
Karthik Velayutham <vkarthik@ponder.io>
2023-03-23 15:48:21 -0500

    add some dtype checks

--- modin/pandas/base.py
+++ modin/pandas/base.py
@@ -2082,8 +2082,14 @@ class BasePandasDataset(ClassLogger):
         """
         Percentage change between the current and a prior element.
         """
+        # Attempting to match pandas error behavior here
         if not isinstance(periods, int):
             raise ValueError(f"periods must be an int. got {type(periods)} instead")
+
+        # Attempting to match pandas error behavior here
+        for dtype in self._get_dtypes():
+            if not is_numeric_dtype(dtype):
+                raise TypeError(f"unsupported operand type for /: got {dtype}")
 
         return self.__constructor__(
             query_compiler=self._query_compiler.pct_change(
