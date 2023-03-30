607e19706fa69cb23713519ca0e15e793bf0521b
Karthik Velayutham <vkarthik@ponder.io>
2023-03-30 14:25:42 -0500

    Update modin/pandas/groupby.py
    Co-authored-by: Mahesh Vashishtha <mvashishtha@users.noreply.github.com>

--- modin/pandas/groupby.py
+++ modin/pandas/groupby.py
@@ -678,7 +678,7 @@ class DataFrameGroupBy(ClassLogger):
         return self.fillna(limit=limit, method="bfill")
 
     def idxmin(self, axis=0, skipna=True, numeric_only=True):
-        if not all(d != np.dtype("O") for d in self._df.dtypes):
+        if any(d == np.dtype("O") for d in self._df.dtypes):
             raise TypeError("reduce operation 'argmin' not allowed for this dtype")
 
         return self._wrap_aggregation(
